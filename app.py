529e3b0df96cabbccca00063ed255480abb737bd
NABIN KUMAR UPADHAYA <104481334+phantombeast7@users.noreply.github.com>
2024-06-09 15:53:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import google.generativeai as genai
 
 app = Flask(__name__)
 
+
 def get_wikimedia_image(query):
     try:
         # Base URL for Wikimedia Commons API
@@ -42,24 +43,24 @@ def get_wikimedia_image(query):
     except Exception as e:
         print(f"Error getting Wikimedia image: {e}")
         return {'image_url': None}
-        
-    
+
+
 # Function to perform Google Image Search for the bird
 def get_gemini_info(query):
     # Replace 'YOUR_API_KEY' with your actual Google API key
-    genai.configure(api_key="AIzaSyCDB2nPeHI_Csv9lmsfSH6UK91l-jVXQMc")
+    genai.configure(api_key="AIzaSyD97cclI9EAgdEaFzlDP00tQCGflTsi5n8")
 
     # Initialize a GenerativeModel instance with 'gemini-pro' model
     model = genai.GenerativeModel('gemini-pro')
 
     # Generate content using Gemini
-    response = model.generate_content(f"Generate a summary about the {query} bird in 150 words.and if this is not a bird then dont mention 'bird' in the summary and if it is bird then mention 'bird' in the summary and only accept real birds")
+    response = model.generate_content(
+        f"Generate a summary about the {query} bird in 150 words in the language as the name of the bird.and if this is not a bird then dont mention 'bird' in the summary and if it is bird then mention 'bird' in the summary and only accept real birds")
 
     # Limit the response to approximately 150 words
     limited_response = ' '.join(response.text.split()[:150])
 
     return {'summary': limited_response}  # Assuming Gemini does not return image URLs
-
 
 
 # Define a route for handling bird information
@@ -69,18 +70,18 @@ def get_bird_info():
 
     # Check if the input is a bird name using Gemini
     gemini_info = get_gemini_info(input_bird_name)
-    if "bird" not in gemini_info['summary'].lower():
-        return jsonify({'message': 'Please enter the name of a bird.'}), 400
-
+    print(gemini_info)
 
     bird_image_url = get_wikimedia_image(input_bird_name)
     bird_image_url = bird_image_url['image_url']
     print(f"\nBird image url: {bird_image_url}")
 
     if bird_image_url:
-        return jsonify({'bird_name': input_bird_name, 'summary': gemini_info['summary'], 'image_url': bird_image_url}), 200
+        return jsonify(
+            {'bird_name': input_bird_name, 'summary': gemini_info['summary'], 'image_url': bird_image_url}), 200
     else:
         return jsonify({'message': 'No HD image found for the given bird name.'}), 404
+
 
 if __name__ == '__main__':
     app.run(debug=False, port=8080)  # Run the app on port 8080
